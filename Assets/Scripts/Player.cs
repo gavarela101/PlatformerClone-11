2c1a619828576ab1cb1d713eadf87c4356ed1289
gavarela101 <gabrielvarela055@gmail.com>
2025-04-09 12:45:32 -0700

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float Speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,13 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
+            transform.position += Vector3.left * Speed * Time.deltaTime;
         }
         else
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position += Vector3.right * Speed * Time.deltaTime;
         }
     }
 }
