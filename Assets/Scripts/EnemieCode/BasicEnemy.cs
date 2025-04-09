3bffdc986c408813ca28c27bf4851e36114b65f6
gavarela101 <gabrielvarela055@gmail.com>
2025-04-09 13:45:49 -0700

    enemies

---
+++ Assets/Scripts/EnemieCode/BasicEnemy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Gabriel Varela
+ * updated 4/9/20
+ * Handles all coding for the basic enemy
+*/
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject LeftPoint;
+    public GameObject RightPoint;
+    public int Speed;
+    public bool GoingLeft;
+
+    private Vector3 leftPos;
+    private Vector3 rightPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftPos = LeftPoint.transform.position;
+        rightPos = RightPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //hanles enemy movement
+    private void Move()
+    {
+        if (GoingLeft)
+        {
+            if (transform.position.x <= leftPos.x)
+            {
+                GoingLeft = false;
+            }
+            else
+            {
+                //moves the enemy to the left
+                transform.position += Vector3.left * Time.deltaTime * Speed;
+            }
+        }
+        else
+        { 
+            if (transform.position.x >= rightPos.x)
+            {
+                GoingLeft = true;
+            }
+            else
+            {
+                //moves the enemy to the right
+                transform.position += Vector3.right * Time.deltaTime * Speed;
+            }
+        }
+    }
+}
